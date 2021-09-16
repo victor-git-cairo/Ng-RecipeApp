cf02ba300411ba87b7da81ba197182dd42af8bd1
VictorSandbox <victor.w.cairo@gmail.com>
2021-09-15 21:58:36 -0500

    added forms

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
 import { Post } from '../post.model';
 
 @Component({
@@ -11,10 +13,10 @@ export class PostCreateComponent {
   enteredContent = "";
   @Output() postCreated = new EventEmitter<Post>();  
 
-  onAddPost(){
+  onAddPost(form: NgForm){
     const post:Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     } 
     this.postCreated.emit(post); 
   }  
